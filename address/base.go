8e32ee05d15b34b6575aa1e08a1c112590364647
Hannes Kimara <hanneskimara@gmail.com>
2022-03-09 11:34:46 +0000

    docs: add documentation for base and enterprise addresses

--- address/base.go
+++ address/base.go
@@ -1,20 +1,21 @@
 package address
 
 import (
-	"log"
-
 	"github.com/fivebinaries/go-cardano-serialization/internal/bech32"
 	"github.com/fivebinaries/go-cardano-serialization/network"
 
 	"github.com/fxamacker/cbor/v2"
 )
 
+// BaseAddress contains information of the base address.
+// A base address directly specifies the staking key that should control the stake for that address but can be used for transactions without registering the staking key in advance.
 type BaseAddress struct {
 	Network network.NetworkInfo
 	Payment StakeCredential
 	Stake   StakeCredential
 }
 
+// Bytes returns a  length 57 byte slice represantation of the Address.
 func (b *BaseAddress) Bytes() []byte {
 	bytes := make([]byte, 57)
 	bytes[0] = (byte(b.Payment.Kind) << 4) | (byte(b.Stake.Kind) << 5) | (byte(b.Network.NetworkId) & 0xf)
@@ -23,19 +24,19 @@ func (b *BaseAddress) Bytes() []byte {
 	return bytes
 }
 
+// String returns a bech32 encoded string of the Enterprise Address.
 func (b *BaseAddress) String() string {
-	str, err := bech32.Encode(b.Prefix(), b.Bytes())
+	str, _ := bech32.Encode(b.Prefix(), b.Bytes())
 
-	if err != nil {
-		log.Println(err)
-	}
 	return str
 }
 
+// NetworkInfo returns NetworkInfo{ProtocolMagigic and NetworkId}.
 func (b *BaseAddress) NetworkInfo() *network.NetworkInfo {
 	return &(b.Network)
 }
 
+// Prefix returns the string prefix for the base address. Prefix `addr` for mainnet addresses and `addr_test` for testnet.
 func (b *BaseAddress) Prefix() string {
 	if b.Network.NetworkId == network.TestNet().NetworkId {
 		return "addr_test"
@@ -43,11 +44,13 @@ func (b *BaseAddress) Prefix() string {
 	return "addr"
 }
 
+// MarshalCBOR returns a cbor encoded byte slice of the base address.
 func (b *BaseAddress) MarshalCBOR() ([]byte, error) {
 	return cbor.Marshal(b.Bytes())
 }
 
-func (b *BaseAddress) ToEnterprise() (addr *EnterpriseAddress, err error) {
+// ToEnterprise returns the Enterprise Address from the base address. This is equivalent to removing the stake part of the base address.
+func (b *BaseAddress) ToEnterprise() (addr *EnterpriseAddress) {
 	addr = NewEnterpriseAddress(
 		&b.Network,
 		&b.Payment,
@@ -55,6 +58,16 @@ func (b *BaseAddress) ToEnterprise() (addr *EnterpriseAddress, err error) {
 	return
 }
 
+// ToReward returns the RewardAddress from the base address. This is equivalent to removing the payment part of the base address.
+func (b *BaseAddress) ToReward() (addr *RewardAddress) {
+	addr = NewRewardAddress(
+		&b.Network,
+		&b.Stake,
+	)
+	return
+}
+
+// NewBaseAddress returns a pointer to a new BaseAddress given the network, payment and stake credentials.
 func NewBaseAddress(network *network.NetworkInfo, payment *StakeCredential, stake *StakeCredential) *BaseAddress {
 	return &BaseAddress{
 		Network: *network,
--- address/enterprise.go
+++ address/enterprise.go
@@ -6,11 +6,14 @@ import (
 	"github.com/fxamacker/cbor/v2"
 )
 
+// EnterpriseAddress contains content for enterprise addresses.
+// Enterprise addresses carry no stake rights, so using these addresses means that you are opting out of participation in the proof-of-stake protocol.
 type EnterpriseAddress struct {
 	Network network.NetworkInfo
 	Payment StakeCredential
 }
 
+// Bytes returns a  length 29 byte slice represantation of the Address.
 func (e *EnterpriseAddress) Bytes() []byte {
 	bytes := make([]byte, 29)
 	bytes[0] = 0b01100000 | (byte(e.Payment.Kind) << 4) | (byte(e.Network.NetworkId) & 0xf)
@@ -18,15 +21,18 @@ func (e *EnterpriseAddress) Bytes() []byte {
 	return bytes
 }
 
+// String returns a bech32 encoded string of the Enterprise Address.
 func (e *EnterpriseAddress) String() string {
 	str, _ := bech32.Encode(e.Prefix(), e.Bytes())
 	return str
 }
 
+// NetworkInfo returns NetworkInfo{ProtocolMagigic and NetworkId}.
 func (e *EnterpriseAddress) NetworkInfo() *network.NetworkInfo {
 	return &(e.Network)
 }
 
+// Prefix returns the string prefix for the Enterprise Address. Prefix `addr` for mainnet addresses and `addr_test` for testnet.
 func (e *EnterpriseAddress) Prefix() string {
 	if e.Network.NetworkId == network.TestNet().NetworkId {
 		return "addr_test"
@@ -34,10 +40,12 @@ func (e *EnterpriseAddress) Prefix() string {
 	return "addr"
 }
 
+// MarshalCBOR returns a cbor encoded byte slice of the enterprise address.
 func (e *EnterpriseAddress) MarshalCBOR() (bytes []byte, err error) {
 	return cbor.Marshal(e.Bytes())
 }
 
+// NewEnterpriseAddress returns a pointer to a new Enterprise Address given the network and payment.
 func NewEnterpriseAddress(network *network.NetworkInfo, payment *StakeCredential) *EnterpriseAddress {
 	return &EnterpriseAddress{
 		Network: *network,
